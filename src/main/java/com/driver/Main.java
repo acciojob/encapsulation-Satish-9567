c59b754ed49230f8a69bf2d9df2b72401a88c1d2
Satish-9567 <150673586+Satish-9567@users.noreply.github.com>
2023-12-25 19:07:46 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+        RWOnly RWOnly=new RWOnly();
+        RWOnly.writeOnly("you might seen some error");
+        RWOnly.readOnly();
+    }
 }
