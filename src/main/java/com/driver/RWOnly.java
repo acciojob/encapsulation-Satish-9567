d868742e4ce772365d3fc8ce6cc7b1653c585fa0
Satish-9567 <150673586+Satish-9567@users.noreply.github.com>
2023-12-25 19:07:20 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    public void writeOnly(String name){
+        this.name=name;
+    }
+    public void readOnly(){
+        System.out.println(this.name);
+    }
 }
