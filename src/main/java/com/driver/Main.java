c226716a2c80810266656a1230556467d43c28d3
Satish-9567 <150673586+Satish-9567@users.noreply.github.com>
2023-12-29 13:41:07 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,9 +1,11 @@
 package com.driver;
 
 public class Main {
-  public static void main(String[] args) {
+    public static void main(String[]args)
+    {
         RWOnly rw=new RWOnly();
-        rw.writeOnly("Satish");
-        System.out.print(rw.readOnly());
+        rw.setName("Satish");
+        System.out.println(rw.getName());
     }
+  
 }
